ffe6250632cdfe1879f4f7ba307a1ab642e51163
Tenekonate <konate-tene@hotmail.fr>
2021-04-14 22:47:19 +0200

    PHPFOL-5-chore: Add phpunit and update makefile and first test

---
+++ tests/UnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function testUnit(): void
+    {
+        $test = true;
+        self::assertTrue($test);
+    }
+}
